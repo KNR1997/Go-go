1be21ae2571604c512c11ec45239d14144f1b65b
KNR1997 <kethakaranasinghe@gmail.com>
2026-01-05 01:31:12 +0530

    feat(migrations): create_user

---
+++ internal/adapters/postgresql/migrations/00005_create_users.sql
@@ -0,0 +1,14 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE users (
+    id BIGSERIAL PRIMARY KEY,
+    email TEXT NOT NULL UNIQUE,
+    password_hash TEXT NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE users;
+-- +goose StatementEnd
